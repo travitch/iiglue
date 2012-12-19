a1c474e1fb73e963bae03f336ca87a71770ef7dd
Tristan Ravitch <travitch@cs.wisc.edu>
2012-12-19 14:49:36 -0600

    Type issues for the Transfer analysis

--- tools/IIGlue.hs
+++ tools/IIGlue.hs
@@ -140,7 +140,7 @@ dump opts name m = do
       phase1Res = parallelCallGraphSCCTraversal cg phase1Func res0
       -- The transferRes includes (builds on) the phase1Res.  The
       -- transfer analysis depends on finalizers (and maybe escape)
-      transferRes = identifyTransfers funcLikes ds pta phase1Res transferSummary
+      transferRes = identifyTransfers funcLikes ds pta phase1Res finalizerSummary transferSummary
       -- Phase2 depends on the results of the transfer analysis (and
       -- the error analysis)
       phase2 :: [ComposableAnalysis AnalysisSummary FunctionMetadata]
